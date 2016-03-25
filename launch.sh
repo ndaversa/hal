401060e56ebd8aa4f9a2c2cb9dadffd4e472d344
Nino D'Aversa <nino@ndaversa.com>
2016-03-25 16:24:30 -0400

    Properly read the env file and load variables for local development (without containers)

--- launch.sh
+++ launch.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-set -o allexport
-source pal.env
+
+while read -r line; do
+  eval `printf "export %s='%s';" "$(echo $line | cut -d'=' -f1)" "$(echo $line | cut -d'=' -f2-)"`
+done < pal.env
 unset REDIS_URL
-set +o allexport
 
 bin/hubot --adapter slack
