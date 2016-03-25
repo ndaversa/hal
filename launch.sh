5b3faeedbd2415be71d86a5d9a650e22ecc740ee
Nino D'Aversa <nino@ndaversa.com>
2016-03-25 16:58:59 -0400

    Even better launch.sh

--- launch.sh
+++ launch.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-regex="^(.+)=(.+)$"
-while IFS='' read -r line; do
-  if [[ $line =~ $regex ]]
-  then
-    export "${BASH_REMATCH[1]}=${BASH_REMATCH[2]}"
-  else
-    echo "Unable to process $line"
-  fi
-done < pal.env
+while IFS='' read -r line; do export "$line"; done < pal.env
 unset REDIS_URL
 
 bin/hubot --adapter slack
